0e229ab880dfaacce1d41ee5fcccffd599c5bc4b
elia_nastasio <eliaclement.nastasio@mail.polimi.it>
2023-12-14 23:56:50 +0100

    giovedì sera fisso

--- exercise_1.m
+++ exercise_1.m
@@ -3,32 +3,75 @@
 data = load("hand.mat");
 original_imm = data.J;
 %equalized_imm = adapthisteq(original_hand);
-smoothed_imm = medfilt2(original_imm);
-%adjusted_imm = imadjust(smoothed_imm,[0.5 0.55], [0.7 0.75]);
-adjusted_imm = adapthisteq(smoothed_imm);
+%smoothed_imm = medfilt2(original_imm);
+%smoothed_imm = imboxfilt(original_imm, 3);
+A = 1.7;
+kernel_size =11;
+blurred = imgaussfilt(original_imm, kernel_size);
+high_boost = A * double(original_imm) - double(blurred);
+high_boost = uint8(max(0, min(255, high_boost)));
+adjusted_imm = imadjust(high_boost,[0 0.55]);
+smoothed_imm = imboxfilt(adjusted_imm, 3);
+%
+
+%
+edge_imm = smoothed_imm;
 % Display the transformed image
 figure(1)
-enhanced_imm = adjusted_imm;
-imshowpair(original_imm,adjusted_imm,"montage")
-title('Original and Edge Enhancement');
-[Gmag,Gdir] = imgradient(adjusted_imm,"prewitt");
-figure(3)
-imshowpair(Gmag,Gdir,"montage");
-% STEP 2
-%robinson_adjusted = adapthisteq(edge_robinson);
-%edge_imm = imadjust(smoothed_imm);
+subplot(2,3,1)
+imshow(original_imm)
+title("Original")
+subplot(2,3,2)
+imshow(high_boost)
+title("High Boost of smoothed")
+subplot(2,3,3)
+imshow(adjusted_imm)
+title("Adjusted")
+subplot(2,3,4)
+imshow(smoothed_imm)
+title("Smoothed")
+subplot(2,3,5)
+%
+subplot(2,3,6)
+imshow(edge_imm)
+title("Final Edge Enhancement")
+% STEP 2 - Robinson and Roberts
 compass_masks = [-1 -2 -1; 0 0 0; 1 2 1]; 
 edge_robinson = imfilter(adjusted_imm, compass_masks, 'conv');
-threshold = 0.006; 
-sigma = 10;
-edge_canny = edge(adjusted_imm,'canny',threshold, sigma);
+threshold = 0.045; 
+edge_roberts = edge(edge_imm, 'Roberts',threshold,'nothinning');
 figure(2)
 subplot(1,2,1)
 imshow(edge_robinson)
 title("Robinson")
 subplot(1,2,2)
-imshow(edge_canny)
-title("Canny")
-%subplot(1,4,4)
-%imshow(robinson_adjusted)
-%title("Robinson hist adjusted")
+imshow(edge_roberts)
+title("Roberts")
+% STEP 3 - Morphological Operators
+% Robinson
+% Robert
+se_1 = strel("disk",3);
+se_2 = strel("line",3,0);
+robert_close = bwareaopen(edge_roberts,15);
+robert_close = bwfill(robert_close,"holes",4);
+robert_open = imopen(robert_close, se_2);
+figure(4)
+subplot(1,3,1)
+imshow(edge_roberts)
+title("Original Roberts")
+subplot(1,3,2)
+imshow(robert_close)
+title("Close Robert")
+subplot(1,3,3)
+imshow(robert_open)
+title("Open Robert")
+% STEP 4 - Visualize Results
+robert_boundaries = bwboundaries(robert_open);
+figure(6)
+imshow(original_imm);
+hold on;
+for k = 1:length(robert_boundaries)
+   boundary = robert_boundaries{k};
+   plot(boundary(:,2), boundary(:,1), 'r')
+end
+hold off
--- exercise_2.m
+++ exercise_2.m
@@ -5,30 +5,12 @@ gray_imm = rgb2gray(original_imm);
 smoothed_imm = imboxfilt(gray_imm, 15);
 edge_imm = edge(smoothed_imm,'prewitt',0.007,'both');
 se = strel('disk',5);
-close_imm = imclose(edge_imm,se);
-stats = regionprops(close_imm, 'BoundingBox', 'Area');
+closed_imm = imclose(edge_imm,se);
+stats = regionprops(closed_imm, 'BoundingBox', 'Area');
 [maxArea, idx] = max([stats.Area]);
 bounding_box = stats(idx).BoundingBox;
 aspect_ratio = bounding_box(3) / bounding_box(4);
 disp(['Aspect Ratio: ' num2str(aspect_ratio)]);
-%{
-figure(1)
-subplot(1,4,1)
-imshow(gray_imm)
-title("Original")
-subplot(1,4,2)
-imshow(edge_imm)
-title("Edges")
-subplot(1,4,3)
-imshow(close_imm)
-title("Closed")
-subplot(1,4,4)
-imshow(gray_imm)
-hold on
-rectangle("position",bounding_box,"EdgeColor",'r')
-hold off
-title("Boudning box")
-%}
 % STEP 2
 gray_cropped_imm = imcrop(gray_imm, bounding_box);
 adjusted_imm = imadjust(gray_cropped_imm, [0 0.68]);
@@ -36,11 +18,6 @@ hist_imm = adapthisteq(adjusted_imm);
 contrast_imm = localcontrast(hist_imm, 0.2, 0.6);
 adjusted_imm_2 = imadjust(contrast_imm, [0 0.6]);
 hist_imm_2 = adapthisteq(adjusted_imm_2);
-
-%{
-sharpened_image = imsharpen(adjusted_image);
-contrast_imm = localcontrast(adjusted_image, 0.4, 0.7);
-%}
 figure(2)
 subplot(1,6,1)
 imshow(gray_cropped_imm)
@@ -60,53 +37,59 @@ title("Second Adjust")
 subplot(1,6,6)
 imshow(adjusted_imm_2)
 title("Second Hist Eq")
-%{
-subplot(1,6,3)
-imshow(adjusted_image)
-title("Adjusted")
-subplot(1,6,4)
-imshow(adapt_equalized_imm)
-title("Adapt Hist")
-subplot(1,6,5)
-imshow(sharpened_image)
-title("Sharpened")
-subplot(1,6,6)
-imshow(contrast_imm)
-title("Local Contrast Processing")
-%}
 % STEP 3
-%T = graythresh(adjusted_imm);
 bin_imm = imbinarize(hist_imm_2, 0.7);
 bin_imm = not(bin_imm);
 se = strel("disk",3);
 close_imm = imclose(bin_imm, se);
-close_back_imm = close_imm;
-background_region = regionprops(close_imm, 'Area', 'PixelIdxList');
-[~, idx_bckground] = max([background_region.Area]);
-biggest_region = zeros(size(close_start_imm));
-biggest_region(background_region(idx).PixelIdxList) = 1;
-biggest_region_inverse = ~biggest_region;
-close_imm(biggest_region_inverse) = 0;
+connected_elements = bwconncomp(close_imm);
+max_area_index = 0;
+max_area_element = 0;
+for i = 1:connected_elements.NumObjects
+    area_element = numel(connected_elements.PixelIdxList{i});
+    if area_element > max_area_element
+        max_area_element = area_element;
+        max_area_label = i;
+    end
+end
+close_background_imm = close_imm;
+close_background_imm(connected_elements.PixelIdxList{max_area_label}) = 0;
+small_elements_threshold = 300;
+close_background_imm = bwareaopen(close_background_imm,small_elements_threshold);
+min_distance_top = find(any(close_background_imm, 2), 1, 'first') - 1;
+min_distance_bottom = size(close_background_imm, 1) - find(any(close_background_imm, 2), 1, 'last');
+min_distance_left = find(any(close_background_imm, 1), 1, 'first') - 1;
+min_distance_right = size(close_background_imm, 2) - find(any(close_background_imm, 1), 1, 'last');
+if min_distance_top > min_distance_bottom
+    trimmed_imm = close_background_imm(min_distance_top - min_distance_bottom + 1:end, :);
+else
+    trimmed_imm = close_background_imm(1:end - min_distance_bottom + min_distance_top, :);
+end
+if min_distance_left > min_distance_right
+    trimmed_imm = trimmed_imm(:, min_distance_left - min_distance_right + 1:end);
+else
+    trimmed_imm = trimmed_imm(:, 1:end - min_distance_right + min_distance_left);
+end
 figure(3)
-subplot(1,3,1)
+subplot(1,4,1)
 imshow(bin_imm)
 title("Binary")
-subplot(1,3,2)
-imshow(close_start_imm)
-title("Closed")
-subplot(1,3,3)
+subplot(1,4,2)
 imshow(close_imm)
-title("Closed with no Background")
-%
-boundaries = bwboundaries(close_imm);
+title("Closed")
+subplot(1,4,3)
+imshow(close_background_imm)
+title("Closed no Background")
+subplot(1,4,4)
+imshow(trimmed_imm)
+title("Trimmed")
+boundaries = bwboundaries(trimmed_imm);
 circularities = zeros(length(boundaries), 1);
 min_length_threshold = 200;
-max_length_threshold = (size(close_imm,1) * 2 + size(close_imm,2) * 2) * 0.9;
-% Loop through boundaries
+max_length_threshold = (size(trimmed_imm,1) * 2 + size(trimmed_imm,2) * 2) * 0.5;
 for k = 1:length(boundaries)
     boundary = boundaries{k};
-    if length(boundary) > min_length_threshold & length(boundary) < max_length_threshold
-        % Compute circularity
+    if length(boundary) > min_length_threshold && length(boundary) < max_length_threshold
         area = polyarea(boundary(:, 2), boundary(:, 1));
         perimeter = length(boundary);
         circularities(k) = (4 * pi * area) / (perimeter^2);
@@ -114,97 +97,46 @@ for k = 1:length(boundaries)
         circularities(k) = 0;
     end
 end
-
-% Find the indices of the first two elements with max circularity
-[sorted_circularities, sorted_indices] = sort(circularities, 'descend');
-N = 1;
-max_circularity_indices = sorted_indices(1:N);
-
-% Display the original image with the identified objects
-figure;
-imshow(close_imm);
-hold on;
-%bounding_box_pills = zeros(200, 1);
-filled_pills = 0;
+[~, max_circularity_index] = max(circularities);
+max_circularity_boundary = boundaries{max_circularity_index};
+min_x = min(max_circularity_boundary(:, 2));
+max_x = max(max_circularity_boundary(:, 2));
+min_y = min(max_circularity_boundary(:, 1));
+max_y = max(max_circularity_boundary(:, 1));
+pill_y = max_y - min_y; 
+pill_x = max_x - min_x;
+y_divisions = floor(size(trimmed_imm, 1) / (pill_y * 1.1));
+x_divisions = floor(size(trimmed_imm, 2) / (pill_x * 1.1));
+x_size = floor(size(trimmed_imm, 2)/x_divisions);
+y_size = floor(size(trimmed_imm, 1)/y_divisions);
+threshold_region_size = 400;
+threshold_region_count = 3;
 empty_pills = 0;
-for i = 1:N
-    % Plot the boundary of the object
-    max_circularity_boundary = boundaries{max_circularity_indices(i)};
-    min_x = min(max_circularity_boundary(:, 2));
-    max_x = max(max_circularity_boundary(:, 2));
-    min_y = min(max_circularity_boundary(:, 1));
-    max_y = max(max_circularity_boundary(:, 1));
-    pill_y = max_y - min_y; 
-    pill_x = max_x - min_x;
-    y_divisions = floor(size(close_imm, 1) / (pill_y*1.1));
-    x_divisions = floor(size(close_imm, 2) / (pill_x*1.1));
-    rect_width = floor(size(close_imm, 2) / x_divisions);
-    rect_height = floor(size(close_imm, 1) / y_divisions);
-
-    for j = 1:x_divisions
-        for k = 1:y_divisions
-            % Calculate the position of the rectangle with respect to (0, 0) of the image
-            rect_x_start = (j - 1) * rect_width + 1;
-            rect_y_start = (k - 1) * rect_height + 1;
-            
-            % Ensure that the indices stay within bounds
-            rect_x_end = rect_x_start + rect_width;
-            rect_y_end = rect_y_start + rect_height;
-            
-            % Extract the sub-image within the rectangle
-            sub_image = close_imm(rect_y_start:rect_y_end, rect_x_start:rect_x_end);
-
-            % Count the number of elements in the sub-image
-            stats_3 = regionprops(sub_image, 'Area');
-
-            % Set a threshold for the number of elements
-            threshold = 3000; % Adjust this threshold as needed
-            num_elements = sum([stats_3.Area] > threshold);
-            % Color the rectangle based on the number of elements
-            if num_elements > 4
-                empty_pills = empty_pills + 1;
-                rectangle('Position', [rect_x_start, rect_y_start, rect_width, rect_height], 'EdgeColor', 'r');
-            else
-                filled_pills = filled_pills + 1;
-                rectangle('Position', [rect_x_start, rect_y_start, rect_width, rect_height], 'EdgeColor', 'g');
-            end
-            % Plot the rectangle
-            %rectangle('Position', [rect_x, rect_y, rect_width, rect_height], 'EdgeColor', 'b');
-        end
-    end
-
-    plot(max_circularity_boundary(:, 2), max_circularity_boundary(:, 1), 'r', 'LineWidth', 2);
-    %rectangle('Position', [min_x, min_y, max_x - min_x, max_y - min_y], 'EdgeColor', 'b');
-end
-
-hold off;
-title('Objects with Top 2 Circularities and Satisfying Length Threshold');
-disp(['Filled Pills: ' num2str(filled_pills)]);
-disp(['Empty Pills: ' num2str(empty_pills)]);
-%{
-cc = bwconncomp(thin_imm);
-stats = regionprops(cc, 'Area', 'Eccentricity', 'BoundingBox');
-unusedPillAreaThreshold = 500;
-usedPillEccentricityThreshold = 0.9;
-unusedPillMask = false(size(thin_imm));
-usedPillMask = false(size(thin_imm));
-for i = 1:length(stats)
-    if stats(i).Area > unusedPillAreaThreshold && stats(i).Eccentricity < usedPillEccentricityThreshold
-        unusedPillMask(cc.PixelIdxList{i}) = true;
-    elseif stats(i).Eccentricity >= usedPillEccentricityThreshold
-        usedPillMask(cc.PixelIdxList{i}) = true;
-    end
-end
+full_pills = 0;
 figure(4);
-subplot(1, 3, 1);
-imshow(thin_imm);
-title('Thin');
-
-subplot(1, 3, 2);
-imshow(unusedPillMask);
-title('Unused Pill Mask');
-
-subplot(1, 3, 3);
-imshow(usedPillMask);
-title('Used Pill Mask');
-%}
+imshow(trimmed_imm);
+hold on;
+for i = 1:x_divisions
+    for j = 1:y_divisions
+        x_center = floor(x_size/2) + x_size * (i - 1);
+        y_center = floor(y_size/2) + y_size * (j - 1);
+        
+        sub_imm = trimmed_imm(y_center - floor(pill_y/2):y_center + floor(pill_y/2), x_center - floor(pill_x/2):x_center + floor(pill_x/2));
+        
+        sub_regions = regionprops(sub_imm, 'Area');
+        num_elements = sum([sub_regions.Area] > threshold_region_size);
+        
+        if num_elements > threshold_region_count
+            empty_pills = empty_pills + 1;
+            rectangle("Position", [x_center - floor(pill_x/2), y_center - floor(pill_y/2), floor(pill_x), floor(pill_y)], 'EdgeColor', 'r')
+        else
+            full_pills = full_pills + 1;
+            rectangle("Position", [x_center - floor(pill_x/2), y_center - floor(pill_y/2), floor(pill_x), floor(pill_y)], 'EdgeColor', 'g')
+        end
+        plot(x_center, y_center, "Marker", "+", "Color", "b", "MarkerSize", 5)
+    end
+end
+hold off;
+title('Subregions');
+disp(['Full Pills: ' num2str(full_pills)]);
+disp(['Empty Pills: ' num2str(empty_pills)]);
